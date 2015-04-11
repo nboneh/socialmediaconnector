6e0cf170d3c13091e2485ac5d1e589be28492786
Nir Boneh <nir.boneh@colorado.edu>
2015-04-11 12:36:32 -0600

    Database works locally

--- index.js
+++ index.js
@@ -18,14 +18,14 @@ var pg = require('pg');
 
 
 app.get('/db', function (request, response) {
-  pg.connect(process.env.DATABASE_URL || "postgres://qyczllslpshilf:C5b-rPxAiZ88wuAeqSsdbe_l2s@ec2-107-20-159-103.compute-1.amazonaws.com:5432/dd8b76iderq0v1"
+  pg.connect(process.env.DATABASE_URL || "postgres://qyczllslpshilf:C5b-rPxAiZ88wuAeqSsdbe_l2s@ec2-107-20-159-103.compute-1.amazonaws.com:5432/dd8b76iderq0v1?ssl=true"
  , function(err, client, done) {
     client.query('SELECT * FROM test_table', function(err, result) {
       done();
       if (err)
        { console.error(err); response.send("Error " + err); }
       else
-       { response.send(process.env.DATABASE_URL); }
+       { response.send(result.rows); }
     });
   });
 })
