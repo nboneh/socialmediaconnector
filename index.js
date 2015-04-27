02d6c781b91072c337461d0fc6841b9ec6413961
mazdeh <vahid.mazdeh@gmail.com>
2015-04-27 16:03:17 -0600

    code works, is readable and pretty.

--- index.js
+++ index.js
@@ -6,13 +6,11 @@ var pg = require('pg');
 var crypto = require('crypto');
 var session = require('client-sessions');
 
-
-
 app.use(session({
-  cookieName: 'session',
-  secret: '4206942069sixtynine',
-  duration: 30 * 60 * 1000,
-  activeDuration: 5 * 60 * 1000,
+    cookieName: 'session',
+    secret: '4206942069sixtynine',
+    duration: 30 * 60 * 1000,
+    activeDuration: 5 * 60 * 1000,
 }));
 
 app.set('port', (process.env.PORT || 5000));
@@ -21,105 +19,88 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 
-var fly = function(messageId){
-  var queryText = "SELECT id FROM Users WHERE NOT message_received_list @> ARRAY[" + messageId +  " ] AND NOT message_sent_list @>  ARRAY[" + messageId + "] ORDER BY RANDOM() LIMIT 2"
-  pg.connect(DB_URL
- , function(err, client, done) {
-   client.query(queryText, function(err, result) {
-      done();
-      if (err)
-       { console.error(err);  }
-      else
-       { 
-        var rows = result.rows;
-        for(i  in rows){
-            var userId = rows[i].id;
-            console.log(userId)
-           var sendQuery = "UPDATE Users set message_received_list = array_append(message_received_list, $1) WHERE id=" + userId;
-           console.log(sendQuery)
-
-            client.query(sendQuery, [messageId] ,function(err, result) {
-                 if (err){ console.error(err);  }
-                 else {console.log("IT IS ALRIGHT")}
-            })
-
-        }
-       }
-     })
- })
+var login = function (req, res){
+    
+    var userName = req.body.userName;
+    var password = req.body.password;
+    var hashpass = crypto.createHash("md5").update(password).digest('hex');
+    var queryText = 'select id from Users where username=\'' + userName + '\' and hashpass=\'' + hashpass + '\'';
+    
+    // console.log(queryText)
+    
+    pg.connect(DB_URL, function(err, client, done) {
+        client.query(queryText, function(err, result) {
+            done();
+            
+            if (err) {
+                console.error(err);
+            }
+            else {
+                if (result.rows[0] != undefined) { 
+                    console.log("Successful Login");
+                    req.session.user = userName; 
+                    req.session.user_id = result.rows[0].id;
+                    res.redirect("/");
+                } 
+                else { 
+                    console.error("Fail Login!")
+                    res.redirect("/")
+                }
+            }
+        });
+    });
 }
 
-var login = function (req, res){
-  var userName = req.body.userName;
-  var password = req.body.password;
-  var hashpass = crypto.createHash("md5").update(password).digest('hex');
-  var queryText = 'select id from Users where username=\'' + userName + '\' and hashpass=\'' + hashpass + '\'';
-  console.log(queryText)
-    pg.connect(DB_URL
- , function(err, client, done) {
-   client.query(queryText, function(err, result) {
-      done();
-      if (err)
-       { console.error(err);  }
-      else
-       { 
-        if(result.rows[0] != undefined)
-        { 
-          console.log("Successful Login");
-          req.session.user  = userName; 
-          req.session.user_id =result.rows[0].id;
-          res.redirect("/");
-        } 
-    else 
-      { console.error("Fail Login!")
-        res.redirect("/")}
-    }
-    });
-  });
+var fly = function(messageId) {
+
+    var queryText = "SELECT id FROM Users WHERE NOT message_received_list @> ARRAY[" + messageId +  " ] AND NOT message_sent_list @>  ARRAY[" + messageId + "] ORDER BY RANDOM() LIMIT 2"
+  
+    pg.connect(DB_URL, function(err, client, done) {
+        client.query(queryText, function(err, result) {
+            done();
+          
+            if (err) { 
+                // handle error
+                console.error(err);
+            }
+          
+            else { 
+                var rows = result.rows;
+                for (i in rows) {
+                    var userId = rows[i].id; 
+                    console.log(userId)
+                    var sendQuery = "UPDATE Users set message_received_list = array_append(message_received_list, $1) WHERE id=" + userId;
+                    console.log(sendQuery)
+                      
+                    client.query(sendQuery, [messageId] ,function(err, result) {
+                        if (err) {
+                          // handle error
+                          console.error(err);
+                        }
+                        else {
+                          console.log("IT IS ALRIGHT")
+                        }
+                    })
+                }
+            }
+
+        })
+    })
 }
 
 
 app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
-
-
-app.get('/session.json', function (req, res) {
-   res.setHeader('Content-Type', 'application/json');
-    res.send(req.session);
+    console.log("Node app is running at localhost:" + app.get('port'))
 })
 
 app.post('/login', function (req, res) {
-   login( req, res);
+    login(req, res);
 })
 
-app.post('/message', function(req, res){
-  var message = req.body.message;
-  var user_id = req.session.user_id;
-
-  var messageQuery = "INSERT INTO messages(content, user_id, user_list) VALUES($1, $2,$3) RETURNING id"
-
-  pg.connect(DB_URL,
-  function(err, client, done) {
-    client.query(messageQuery, [message, user_id, [user_id]], function(err, result) {
-      if(err){
-       }
-      else {
-        messageId = result.rows[0].id
-        var userQuery = "UPDATE Users set message_sent_list = array_append(message_sent_list, $1) WHERE id=" + user_id;
-        client.query(userQuery, [messageId], function(err, result){
-          if(err){
-          }
-          else {
-            // PASSITON FUNCTION
-            fly(messageId)
-          }
-        })
-      } // first else
-    }) // query closed
-   })
+app.get('/session.json', function (req, res) {
+    res.setHeader('Content-Type', 'application/json');
+    res.send(req.session);
 })
-
 
 app.delete('/logout', function (req, res) {
     req.session.reset();
@@ -127,37 +108,66 @@ app.delete('/logout', function (req, res) {
     res.redirect(303, '/')
 })
 
+app.post('/message', function(req, res){
+    
+    var message = req.body.message;
+    var user_id = req.session.user_id;
+    var messageQuery = "INSERT INTO messages(content, user_id, user_list) VALUES($1, $2,$3) RETURNING id"
+
+    pg.connect(DB_URL, function(err, client, done) {
+        client.query(messageQuery, [message, user_id, [user_id]], function(err, result) {
+            if(err) {
+                //hadnle error
+            }
+            else {
+                messageId = result.rows[0].id
+                var userQuery = "UPDATE Users set message_sent_list = array_append(message_sent_list, $1) WHERE id=" + user_id;
+                
+                client.query(userQuery, [messageId], function(err, result) {
+                    if(err){
+                    // handle error
+                    }
+                    else {
+                    fly(messageId)
+                    }
+                })
+            }
+        })
+    })
+})
+
 
 app.post('/register', function (req, res) {
 
-var user = req.body.userName;
-var hashpass = crypto.createHash("md5").update(req.body.password).digest('hex');
-console.log(hashpass);
-var queryText = "INSERT INTO Users(username,hashpass) VALUES($1, $2) RETURNING id"
- 
- pg.connect(DB_URL
- , function(err, client, done) {
-  client.query(queryText, [user, hashpass], function(err, result){
-    if(err){
-      console.error("ERROR REGISTERING: " + err) 
-    }
-    else {
-      login(req,res);
-    }
-  })
- })
+    var user = req.body.userName;
+    var hashpass = crypto.createHash("md5").update(req.body.password).digest('hex');
+    var queryText = "INSERT INTO Users(username,hashpass) VALUES($1, $2) RETURNING id"
+     
+    pg.connect(DB_URL, function(err, client, done) {
+        client.query(queryText, [user, hashpass], function(err, result) {
+            if(err) {
+                // handle error
+                console.error("ERROR REGISTERING: " + err) 
+            }
+            else {
+                login(req,res);
+            }
+        })
+    })
 })
 
 
 app.get('/db', function (request, response) {
-  pg.connect(DB_URL
- , function(err, client, done) {
-    client.query('SELECT * FROM Users', function(err, result) {
-      done();
-      if (err)
-       { console.error(err); response.send("Error " + err); }
-      else
-       { response.send(result.rows); }
+    pg.connect(DB_URL, function(err, client, done) {
+        client.query('SELECT * FROM Users', function(err, result) {
+            done();
+            if (err) { 
+                console.error(err);
+                response.send("Error " + err);
+            }
+            else {
+                response.send(result.rows); 
+            }
+        });
     });
-  });
 })
--- public/react/main.js
+++ public/react/main.js
@@ -1,132 +1,126 @@
-// var data = [
-//   {author: "Pete Hunt", text: "This is one comment"},
-//   {author: "Jordan Walke", text: "This is *another* comment"}
-// ];
-
 
 var MainPage = React.createClass({
+    
+    getInitialState: function() {
+        return {
+            session:{}
+        };
+    },
 
-   getInitialState: function(){
-    return {session:{}};
-  },
-
-  componentDidMount: function(){
-    $.ajax({
-      url: "session.json",
-      dataType: 'json',
-      success: function(data) {
-        this.setState({session: data});
-      }.bind(this),
-      error: function(xhr, status, err) {
-        console.error("Session request failed", status, err.toString());
-
-      }.bind(this)
-    });
-  },
+    componentDidMount: function() {
+        $.ajax({
+            url: "session.json",
+            dataType: 'json',
+            success: function(data) {
+                this.setState({session: data});
+            }.bind(this),
+            error: function(xhr, status, err) {
+                console.error("Session request failed", status, err.toString());
+            }.bind(this)
+        });
+    },
 
     render: function() {
-      if(this.state.session.user == undefined){
-      return(
-           <div className="loginScreen">
-    <LoginBox />
-    <RegisterBox />
-    </div>
-    );
-  } else {
-    return(
-  <div className="mainPage">
-  <h1> {this.state.session.user} </h1>
-  <LogoutButton />
-  <PostItForm />
-  </div>
-  );
-  }
-  } 
-  })
+        if(this.state.session.user == undefined) {
+            return (
+                <div className="loginScreen">
+                    <LoginBox />
+                        <RegisterBox />
+                </div>
+            );
+        } else {
+            return (
+                <div className="mainPage">
+                    <h1> {this.state.session.user} </h1>
+                    <LogoutButton />
+                    <PostItForm />
+                </div>
+            );
+        }
+    }
+})
 
 var LoginBox = React.createClass({
-  render: function() {
-    return (
-      <div className="loginBox">
-        <h1>Login</h1>
-          <form className="onLoginSubmit" method="post" action="/login">
-      <div className="loginForm">
-          User Name: 
-            <input type="text" name="userName" ref="userName" required/>
-            Password: 
-            <input type="password" name="password" ref="password" required/>
 
-      </div>
-          
-
-        <input type="submit" value="Login!" />
-      
-      </form>
-      </div>
-    );
-  }
+    render: function() {
+        return (
+            <div className="loginBox">
+                <h1>Login</h1>
+                    <form className="onLoginSubmit" method="post" action="/login">
+                        <div className="loginForm">
+                            User Name: 
+                            <input type="text" name="userName" ref="userName" required/>
+                            Password: 
+                            <input type="password" name="password" ref="password" required/>
+                        </div>
+                        <input type="submit" value="Login!" />
+                    </form>
+            </div>
+        );
+    }
 });
 
 var RegisterBox =  React.createClass({
-  render: function() {
-    return (
-      <div className="registerBox">
-        <h1>Register</h1>
-          <form className="onRegisterSubmit" method="post" action="/register">
-      <div className="registerForm">
-            User Name: 
-            <input type="text" name="userName" ref="userName" required/>
-            Password: 
-            <input type="password" name="password" pattern=".{6,}" onchange="form.re-password.pattern = this.value;" ref="password" required/>
-            Confirm Password: 
-           <input type="password" pattern=".{6,}" name="re-password" required/>
 
-      </div>
-        <input type="submit" value="Register!" />
-      
-      </form>
-      </div>
-    );
-  }
+    render: function() {
+        return (
+            <div className="registerBox">
+                <h1>Register</h1>
+                <form className="onRegisterSubmit" method="post" action="/register">
+                    <div className="registerForm">
+                        User Name: 
+                        <input type="text" name="userName" ref="userName" required/>
+                        Password: 
+                        <input type="password" name="password" pattern=".{6,}" onchange="form.re-password.pattern = this.value;" ref="password" required/>
+                        Confirm Password: 
+                        <input type="password" pattern=".{6,}" name="re-password" required/>
+                    </div>
+                    <input type="submit" value="Register!" />
+                </form>
+            </div>
+        );
+    }
 });
 
+
 var PostItForm =React.createClass({
+    
     render: function() {
-      return (
-    <div className="messageBox">
-  <form  method="post" action="/message">
-  <textarea rows="4" cols="50" name="message" pattern=".{5,255}" ref="message"></textarea>
-   <input type="submit" value="Submit!" />
-      
-    </form>
-    </div>
-    );
-  }
+        return (
+            <div className="messageBox">
+                <form  method="post" action="/message">
+                    <textarea rows="4" cols="50" name="message" pattern=".{5,255}" ref="message"></textarea>
+                    <input type="submit" value="Submit!" />  
+                </form>
+            </div>
+        );
+    }
 });
 
 var LogoutButton = React.createClass({
-  logout: function(){
-    $.ajax({
-    url: '/logout',
-    type: 'DELETE',
-    success: function(result) {
-         window.location = '/';
-    }
-  });
-  },
 
-   render: function() {
-      return (
+    logout: function() {
+        $.ajax({
+            url: '/logout',
+            type: 'DELETE',
+            success: function(result) {
+                 window.location = '/';
+            }
+        });
+    },
+
+    render: function() {
+        return (
             <div className="logoutButton">
-                  <button onClick={this.logout}>Logout!</button>
-         </div>
-    );
-  }
+                <button onClick={this.logout}>Logout!</button>
+            </div>
+        );
+    }
+
 });
 
 
-
 React.render(
-  <MainPage />,
-  document.getElementById('content')
+    <MainPage />,
+    document.getElementById('content')
 );
